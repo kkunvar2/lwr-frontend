28de4006ef4fdb82c8d1316cd21f9f934e8eebf8
kkunvar2 <kkunvar2@gmail.com>
2024-04-05 12:19:50 +0530

    try to role based

--- src/Services/authService.js
+++ src/Services/authService.js
@@ -1,3 +1,6 @@
+import { Outlet, useNavigate } from "react-router-dom";
+import { useEffect } from "react";        
+
 // saveUser
 export const saveLoggedInUser = (inputs) => {
     sessionStorage.setItem('authenticatUser', inputs)
@@ -26,3 +29,60 @@ export const logoutUser = () => {
     localStorage.clear();
     sessionStorage.clear();
 }
+
+//Decide Role
+export const decideRole = () => {
+    let loginData = JSON.parse(localStorage.getItem('token'));
+    console.log(loginData);
+    try {
+        if (loginData && loginData.role === 'ADMIN') {
+            return {
+                auth: true,
+                user: 'ADMIN'
+            };
+        } else if (loginData && (loginData.role === 'hr' || loginData.role === 'EMPLOYEE')) {
+            return {
+                auth: true,
+                user: 'EMPLOYEE'
+            };
+        } else {
+            return {
+                auth: false,
+                user: null
+            };
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+export const PrivateRoutes = () => {
+    const { user, auth } = decideRole();
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (!auth || user !== 'ADMIN') {
+            navigate('/');
+        }
+    }, [user, auth, navigate]);
+
+    return (
+        <div>
+            <Outlet />
+        </div>
+    );
+};
+
+export const PublicRoutes = () => {
+    const { user, auth } = decideRole();
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (!auth || user !== 'EMPLOYEE') {
+            navigate('/');
+        }
+    }, [user, auth, navigate]);
+
+    return (
+        <div>
+            <Outlet />
+        </div>
+    );
+};
